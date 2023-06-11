5b08a9b8fcba566a2a86c5062a32935b19681846
marksikaundi <emmanuelmark117@gmail.com>
2023-06-11 22:44:24 +0200

    added  components

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from "./components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Developer Documents',
-  description: 'Create a developer document site with Next.js and MDX.',
-}
+  title: "Developer Documents",
+  description: "Create a developer document site with Next.js and MDX.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+        <Header />
+
+        {/* Navbar */}
+
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className=''>
-      <h1 className=''>Hello World</h1>
+      <h1 className='text-red-600'>Hello World</h1>
     </div>
   )
 }
