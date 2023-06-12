4e4ea695c4cdf4c59e193b7ef7590dc07b2f329c
marksikaundi <emmanuelmark117@gmail.com>
2023-06-12 11:14:36 +0200

    navarbar components

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,8 +3,8 @@ import NavbarItem from "./NavbarItem";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-center dark:bg-gray-600">
-      <NavbarItem title="Services" param="fetchServices" />
+    <div className="flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4">
+      <NavbarItem title="Projects" param="fetchProjects" />
       <NavbarItem title="Courses" param="fetchCourses" />
     </div>
   );
--- src/app/components/NavbarItem.jsx
+++ src/app/components/NavbarItem.jsx
@@ -1,10 +1,22 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 
+import { useSearchParams } from "next/navigation";
+
 export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams(param);
+  const programs = searchParams.get("programs");
   return (
     <div>
-      <Link href={`/?course=${param}`}>
+      <Link
+        className={`m-4 hover:text-amber-600 font-semibold p-2 ${
+          programs &&
+          programs === param &&
+          "underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+        }}`}
+        href={`/?programs=${param}`}
+      >
         {title}
       </Link>
     </div>
