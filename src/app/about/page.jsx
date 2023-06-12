bf52ed2300a8ae1551f198de4c88af93ac369803
marksikaundi <emmanuelmark117@gmail.com>
2023-06-12 10:08:59 +0200

    About us page completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>
-      About us
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About Us </h1>
+      <h5 className="text-sm">Last updates 06/12/2023 by Mark Sikaundi</h5>
+
+      <p className="">
+        This is home to Otofast Project, Otofast Project is a vibrant community
+        of technology enthusiasts and professionals who are passionate about
+        exploring the latest trends and innovations in the tech industry. With a
+        focus on fostering collaboration, creativity, and learning, the
+        community provides a platform for members to connect with like-minded
+        individuals, share their ideas and expertise, and participate in
+        engaging discussions and events. Whether you are a seasoned tech expert
+        or just starting out in your career, Otofast offers a welcoming and
+        supportive environment where you can learn from others, gain new
+        insights and perspectives, and build meaningful connections with peers
+        and mentors.
+      </p>
+      <p>
+        Members of the community come from a diverse range of backgrounds and
+        fields, including software development, data science, cybersecurity, AI,
+        and more, making it a great place to explore different areas of interest
+        and broaden your horizons. From meetups and hackathons to online forums
+        and social media groups, Otofast offers a range of opportunities for
+        members to connect and engage with the community.
+      </p>
+
+      <p>
+        {" "}
+        Whether you are looking to network, collaborate on a project, or simply
+        learn more about the latest trends and technologies, Otofast is the
+        place to be for anyone passionate about tech. The entire application is
+        Developed on so cool Technologie.
+      </p>
+
+      <div>
+        <h1 className="text-2xl font-medium text-amber-600">Our Team </h1>
+        <p>
+          We are a team of passionate developers, designers, and writers who are
+          committed to creating the best possible experience for our users. We
+          believe that technology should be accessible to everyone, and we
+          strive to make our products as easy to use as possible. Our goal is to
+          provide you with a platform where you can learn, share, and connect
+          with others who share your interests. We hope you enjoy using our
+          products as much as we enjoy building them! Happy Coding
+        </p>
+        <p>
+          For any querry or support, you can reach out to us via email  Email us:{" "}
+          <a href="mailto:info@devcircleafrica.com">info@devcircleafrica.com</a> or visit
+          our community forum at {"forum.devcircleafrica.com "}
+        </p>
+        <hr />
+      </div>
     </div>
-  )
+  );
 }
