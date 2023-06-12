8df67777096e1226967eacc232acf5705a526c20
marksikaundi <emmanuelmark117@gmail.com>
2023-06-12 08:48:33 +0200

    added waitlist home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className=''>
-      <h1 className='text-red-600'>Hello World</h1>
+      <h1 className='text-red-600 text-center text-6xl'>Hello World, Coming Soon</h1>
+      <p className='text-center text-lg'>Currently under Development 👨‍💻 by Our team at DevCircle Africa</p>
     </div>
   )
 }
