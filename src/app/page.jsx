abc902bc6c1f8fc1d73843dca7b4c6e2da249597
marksikaundi <emmanuelmark117@gmail.com>
2023-06-18 23:19:37 +0200

    added a paragrap

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col md:flex-row bg-gray-200 rounded-lg shadow-lg p-8 m-4">
         <div className="md:w-1/2">
-          <h1 className="text-2xl font-bold mb-4">Welcome to Otofast</h1>
+          <h1 className="text-2xl font-bold mb-4">Otofast</h1>
           <p className="text-black">
             From meetups and hackathons to online forums and social media
             groups, Otofast offers a range of opportunities for members to
@@ -19,6 +19,12 @@ export default function Home() {
             network, collaborate on a project, or simply learn more about the
             latest trends and technologies, Otofast is the place to be for
             anyone passionate about tech.
+          </p>
+          <p>
+            Otofast is a community of tech enthusiasts, developers, designers,
+            and entrepreneurs who are passionate about technology. Our mission
+            is to build a community of tech enthusiasts across Africa, and to
+            provide them with the resources they need to grow and succeed.
           </p>
         </div>
         <div className="md:w-1/2 mt-4 md:mt-0">
