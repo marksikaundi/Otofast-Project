4e0184a10a752d232888e10c49f3590bb7f12cd6
marksikaundi <emmanuelmark117@gmail.com>
2023-06-20 22:29:48 +0200

    added some dummy data in courses

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      courses
+    <div className="bg-gray-100 min-h-screen">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold mb-4">Otofast Project Program</h1>
+        <p className="text-gray-700 mb-8">
+          Welcome to the Power Learn Program! This program is designed to help you improve your skills and knowledge in various subjects.
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="bg-white rounded shadow p-4">
+            <h2 className="text-2xl font-bold mb-4">Course 1</h2>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
+            </p>
+          </div>
+          <div className="bg-white rounded shadow p-4">
+            <h2 className="text-2xl font-bold mb-4">Course 2</h2>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
+            </p>
+          </div>
+          <div className="bg-white rounded shadow p-4">
+            <h2 className="text-2xl font-bold mb-4">Course 3</h2>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
+            </p>
+          </div>
+          <div className="bg-white rounded shadow p-4">
+            <h2 className="text-2xl font-bold mb-4">Course 4</h2>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
