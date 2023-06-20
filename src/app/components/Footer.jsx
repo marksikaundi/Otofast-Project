9c5262e29a2a264d875530d4cdacea7b70276dd7
marksikaundi <emmanuelmark117@gmail.com>
2023-06-20 22:04:24 +0200

    added new layout footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      <footer>
-      {/* Footer content */}
-      <p>Otofast Project &copy; {new Date().getFullYear()}</p>
+    // <div>
+    //   <footer>
+    //   {/* Footer content */}
+    //   <p>Otofast Project &copy; {new Date().getFullYear()}</p>
+    // </footer>
+    // </div>
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-xl font-bold">Power Learn Project</h2>
+            <p className="mt-2">Learn and grow with us!</p>
+          </div>
+          <div className="flex space-x-4">
+            <a href="#" className="text-gray-300 hover:text-white">Home</a>
+            <a href="#" className="text-gray-300 hover:text-white">About</a>
+            <a href="#" className="text-gray-300 hover:text-white">Courses</a>
+            <a href="#" className="text-gray-300 hover:text-white">Contact</a>
+          </div>
+        </div>
+        <hr className="my-4 border-gray-700" />
+        <div className="text-sm flex justify-between">
+          <p>&copy; {new Date().getFullYear()} Power Learn Project. All rights reserved.</p>
+          <div className="flex space-x-4">
+            <a href="#" className="text-gray-300 hover:text-white">Privacy Policy</a>
+            <a href="#" className="text-gray-300 hover:text-white">Terms of Service</a>
+          </div>
+        </div>
+      </div>
     </footer>
-    </div>
   )
 }
