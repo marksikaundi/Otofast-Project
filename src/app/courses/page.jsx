e00c3645b536559da0ca45bea3463a337ecc5099
marksikaundi <emmanuelmark117@gmail.com>
2023-06-20 22:46:08 +0200

    updates

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -6,7 +6,7 @@ export default function page() {
       <div className="container mx-auto px-4 py-8">
         <h1 className="text-black text-4xl font-bold mb-4">Otofast Project Program</h1>
         <p className="text-black mb-8">
-          Welcome to the Power Learn Program! This program is designed to help you improve your skills and knowledge in various subjects.
+          Welcome to the Otofast Project! This program is designed to help you improve your skills and knowledge in various subjects.
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <div className="bg-white rounded shadow p-4">
