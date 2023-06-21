c0c4c6660d5301739d89d4c7a749a674f676a2c0
marksikaundi <emmanuelmark117@gmail.com>
2023-06-21 19:04:11 +0200

    changed the footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,20 +1,70 @@
-import React from 'react'
+import React from "react";
+import {
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagramSquare,
+  FaTwitterSquare,
+} from "react-icons/fa";
 
 export default function Footer() {
   return (
-    // <div>
-    //   <footer>
-    //   {/* Footer content */}
-    //   <p>Otofast Project &copy; {new Date().getFullYear()}</p>
-    // </footer>
-    // </div>
-    <div>
-      {/* Footer */}
-      <footer className="bg-gray-200 py-4 px-4">
-        <div className="container mx-auto text-center text-gray-600">
-          <span>&copy; {new Date().getFullYear()} Otofast Project. All rights reserved.</span>
+    <div className="max-w[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 ">
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+          Otofast Project
+        </h1>
+        <p className="py-4">
+          Otofast is a software program designed for hearing care professionals
+          such as audiologists and hearing aid specialists. It is a
+          comprehensive solution that streamlines hearing assessment and hearing
+          aid fitting processes.
+        </p>
+        <div className="flex justify-between md:w-[78%] my-6">
+          <FaFacebookSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaInstagramSquare size={30} />
+          <FaTwitterSquare size={30} />
         </div>
-      </footer>
+      </div>
+      <div className="lg:col-span-2 mt-6 flex justify-between">
+        <div>
+          <h6 className="font-medium text-gray-400">Solution</h6>
+          <ul>
+            <li className="py-2 text-sm">Analytics</li>
+            <li className="py-2 text-sm">Commerce</li>
+            <li className="py-2 text-sm">Marketing</li>
+            <li className="py-2 text-sm">Insight</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Support</h6>
+          <ul>
+            <li className="py-2 text-sm">Pricing</li>
+            <li className="py-2 text-sm">Documentation</li>
+            <li className="py-2 text-sm">Guides</li>
+            <li className="py-2 text-sm">API status</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Company</h6>
+          <ul>
+            <li className="py-2 text-sm">About</li>
+            <li className="py-2 text-sm">Blog</li>
+            <li className="py-2 text-sm">Forum</li>
+            <li className="py-2 text-sm">Careers</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm">Claims</li>
+            <li className="py-2 text-sm">Policy</li>
+            <li className="py-2 text-sm">Terms</li>
+            <li className="py-2 text-sm">Courses</li>
+          </ul>
+        </div>
+      </div>
+      <div className="flex ">Copyrite 2023 Otofast Project</div>
     </div>
-  )
+  );
 }
