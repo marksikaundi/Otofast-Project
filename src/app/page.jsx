e17614f0727114458ac3dfe146ad063c583156da
marksikaundi <emmanuelmark117@gmail.com>
2023-06-25 22:46:50 +0200

    clean ups

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Analytics from "./components/Analytics";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -36,28 +35,6 @@ const Hero = () => {
         </div>
       </div>
     </div>
-    // <div className="text-white">
-    //   <div className="max-w-[800px] mt-[96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-    //     <p className="text-green font-bold p-2">
-    //       GROWING WITH QUANTUM COMPUTING
-    //     </p>
-    //     <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
-    //       Grow with data.
-    //     </h1>
-    //     <div className="flex justify-center items-center">
-    //       <p className="md:text-5xl sm:text-4xl py-4 text-xl font-bold">
-    //         Research, Inn for Big data
-    //       </p>
-    //     </div>
-    //     <p className="md:text-2xl text-xl font-bold text-white">
-    //       Develop your serverless platform worth millions of packages.
-    //     </p>
-    //     <button className="bg-[#00df9a] w-[200px] rounded-md mx-auto font-medium my-6 py-3 text-black">
-    //       Get started
-    //     </button>
-    //   </div>
-    //   <Analytics />
-    // </div>
   );
 };
 
