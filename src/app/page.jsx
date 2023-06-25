d80315a1fd628c599426668657c2709fdb13143a
marksikaundi <emmanuelmark117@gmail.com>
2023-06-25 22:39:55 +0200

    alt

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,6 +12,7 @@ const Hero = () => {
             class="h-48 w-full object-cover md:h-full md:w-48"
             width={100}
             height={100}
+            alt="Apply Now"
             src="https://cdn.pixabay.com/photo/2023/06/09/19/37/letter-8052497_1280.png"
           />
         </div>
