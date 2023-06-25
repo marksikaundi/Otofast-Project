605ebdd6c5568a500e7c2a5029b0d05349d71ed6
marksikaundi <emmanuelmark117@gmail.com>
2023-06-25 21:30:27 +0200

    working on courses

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-black text-4xl font-bold mb-4">Otofast Project Program</h1>
+        <h1 className="text-black text-4xl font-bold mb-4">
+          Otofast Project Program
+        </h1>
         <p className="text-black mb-8">
-          Welcome to the Otofast Project! This program is designed to help you improve your skills and knowledge in various subjects.
+          Our programmes are rigorous and push individuals to their full
+          potential. By the time they graduate, our learners can safely rely on
+          the technical skills they have acquired.{" "}
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <div className="bg-white rounded shadow p-4">
-            <h2 className="text-black text-2xl font-bold mb-4">Software Engineering</h2>
-            <p className="text-black">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
-            </p>
+            <h2 className="text-black text-2xl font-bold mb-4">
+              Software Engineering
+            </h2>
+            <p className="text-black"></p>
           </div>
           <div className="bg-white rounded shadow p-4">
-            <h2 className="text-black text-2xl font-bold mb-4">Deep Learning</h2>
-            <p className="text-black">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
-            </p>
+            <h2 className="text-black text-2xl font-bold mb-4">
+              Deep Learning
+            </h2>
+            <p className="text-black"></p>
           </div>
           <div className="bg-white rounded shadow p-4">
-            <h2 className="text-black text-2xl font-bold mb-4">Software Testing</h2>
-            <p className="text-black">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
-            </p>
+            <h2 className="text-black text-2xl font-bold mb-4">
+              Software Testing
+            </h2>
+            <p className="text-black"></p>
           </div>
           <div className="bg-white rounded shadow p-4">
-            <h2 className="text-black text-2xl font-bold mb-4">Cloud Computing</h2>
-            <p className="text-black">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce gravida, elit ut consequat interdum, lacus tortor fringilla enim, vitae scelerisque nunc ipsum nec orci.
-            </p>
+            <h2 className="text-black text-2xl font-bold mb-4">
+              Cloud Computing
+            </h2>
+            <p className="text-black"></p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
