2e40fbab2623db0df659dab6a1449da327c55806
marksikaundi <emmanuelmark117@gmail.com>
2023-06-27 07:35:38 +0200

    cofigs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['g.foolcdn.com', 'cdn.pixabay.com'],
-    }
-}
+  images: {
+    domains: ["g.foolcdn.com", "cdn.pixabay.com", "tailwindui.com"],
+  },
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
