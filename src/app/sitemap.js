4b6efee34f7074bc848dfc05f49a8e861558da25
marksikaundi <emmanuelmark117@gmail.com>
2023-06-27 16:47:14 +0200

    added sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,39 @@
-export default async function sitemap() {
+export default function sitemap() {
   return [
     {
       url: "https://otofastproject.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/courses",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/features",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/marketplace",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/product",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/code-hacks",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/funding-programs",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://otofastproject.com/about",
       lastModified: new Date(),
     },
   ];
