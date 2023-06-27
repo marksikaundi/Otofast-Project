c76dddd54e7d54348991d51f1dc6307780837b35
marksikaundi <emmanuelmark117@gmail.com>
2023-06-27 07:31:39 +0200

    bug fix

--- src/app/code-hacks/page.jsx
+++ src/app/code-hacks/page.jsx
@@ -4,6 +4,7 @@ import {
   LockClosedIcon,
   ServerIcon,
 } from "@heroicons/react/20/solid";
+import Image from "next/image";
 
 export default function Example() {
   return (
@@ -58,7 +59,7 @@ export default function Example() {
           </div>
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-          <img
+          <Image
             className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
             alt=""
