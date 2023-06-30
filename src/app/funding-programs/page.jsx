2ec9bd420a87a27c39255b379e3fac5e3c2302b0
marksikaundi <emmanuelmark117@gmail.com>
2023-06-30 22:50:38 +0200

    updates

--- src/app/funding-programs/page.jsx
+++ src/app/funding-programs/page.jsx
@@ -56,6 +56,7 @@ export default function Example() {
               <input
                 type="text"
                 name="first-name"
+                placeholder="Enter your first name"
                 id="first-name"
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -73,6 +74,7 @@ export default function Example() {
               <input
                 type="text"
                 name="last-name"
+                placeholder="Enter your last name"
                 id="last-name"
                 autoComplete="family-name"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -81,16 +83,17 @@ export default function Example() {
           </div>
           <div className="sm:col-span-2">
             <label
-              htmlFor="Organization-If Any"
+              htmlFor="Organization-if any"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
-              Organization if Any
+              Organization if any
             </label>
             <div className="mt-2.5">
               <input
                 type="text"
-                name="Organization-If Any"
-                id="Organization-If Any"
+                name="Organization-if any"
+                id="Organization-if any"
+                placeholder="Enter your Organization name if you have"
                 autoComplete="organization"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -108,6 +111,7 @@ export default function Example() {
                 type="email"
                 name="email"
                 id="email"
+                placeholder="Enter your email address"
                 autoComplete="email"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -147,6 +151,7 @@ export default function Example() {
               <textarea
                 name="message"
                 id="message"
+                placeholder="Enter the program you applying here"
                 rows={4}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue={""}
@@ -187,7 +192,7 @@ export default function Example() {
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Connect Now{" "}
+            Submit Now{" "}
           </button>
         </div>
       </form>
