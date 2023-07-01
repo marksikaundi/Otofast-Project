f88d5f4bc01bf92eb66cbf98f10ffe751cc96882
marksikaundi <emmanuelmark117@gmail.com>
2023-07-01 20:51:01 +0200

    improved the header

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -114,7 +114,7 @@ export default function Example() {
         </Dialog>
       </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      {/* <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
@@ -177,7 +177,7 @@ export default function Example() {
             }}
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
