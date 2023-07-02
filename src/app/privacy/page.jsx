0963315d92ef9c515b16ee36cad9a0474d97ace9
marksikaundi <emmanuelmark117@gmail.com>
2023-07-02 22:39:46 +0200

    updates p-t

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
-function page() {
+export default function page() {
   return (
-    <div>
-      privacy
-    </div>
-  )
-}
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-[#00df9a]">Privacy Policy </h1>
+      <h5 className="text-sm">by Mark Sikaundi</h5>
 
-export default page
+      <p className="text-white">
+        Otofast Project is a vibrant community of tech enthusiasts and
+        professionals who are passionate about exploring the latest trends and
+        innovations in the tech industry. With a focus on fostering
+        collaboration, creativity, and learning, the community provides a
+        platform for members to connect with like-minded individuals, share
+        their ideas and expertise, and participate in engaging discussions and
+        events. Whether you are a seasoned tech expert or just starting out in
+        your career, Otofast offers a welcoming and supportive environment where
+        you can learn from others, gain new insights and perspectives, and build
+        meaningful connections with peers and mentors.
+      </p>
+      <p className="text-white">
+        Members of the community come from a diverse range of backgrounds and
+        fields, including software development, data science, cybersecurity, AI,
+        and more, making it a great place to explore different areas of interest
+        and broaden your horizons. From meetups and hackathons to online forums
+        and social media groups, Otofast offers a range of opportunities for
+        members to connect and engage with the community.
+      </p>
+
+      <p className="text-white">
+        {" "}
+        Whether you are looking to network, collaborate on a project, or simply
+        learn more about the latest trends and technologies, Otofast is the
+        place to be for anyone passionate about tech. The entire application is
+        Developed with cool Technologie.
+      </p>
+
+      <div>
+        <h1 className="text-2xl font-medium text-[#00df9a]">Our Team </h1>
+        <p className="text-white">
+          We are a team of passionate developers, designers, and writers who are
+          committed to creating the best possible experience for our users. We
+          believe that technology should be accessible to everyone, and we
+          strive to make our products as easy to use as possible. Our goal is to
+          provide you with a platform where you can learn, share, and connect
+          with others who share your interests. We hope you enjoy using our
+          products as much as we enjoy building them! Happy Coding
+        </p>
+        
+      </div>
+    </div>
+  );
+}
