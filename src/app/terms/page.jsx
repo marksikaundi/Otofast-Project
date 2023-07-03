2c05c0d1bea288e4282f406686e9c3188c7907a7
marksikaundi <emmanuelmark117@gmail.com>
2023-07-03 03:52:15 +0200

    up tc

--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -3,50 +3,155 @@ import React from "react";
 export default function page() {
   return (
     <div className="max-w-6xl mx-auto space-y-4 p-4">
-      <h1 className="text-2xl font-medium text-[#00df9a]">Terms & Condition </h1>
+      <h1 className="text-2xl font-medium text-[#00df9a]">
+        Terms & Condition{" "}
+      </h1>
       <h5 className="text-sm">by Mark Sikaundi</h5>
 
       <p className="text-white">
-        Otofast Project is a vibrant community of tech enthusiasts and
-        professionals who are passionate about exploring the latest trends and
-        innovations in the tech industry. With a focus on fostering
-        collaboration, creativity, and learning, the community provides a
-        platform for members to connect with like-minded individuals, share
-        their ideas and expertise, and participate in engaging discussions and
-        events. Whether you are a seasoned tech expert or just starting out in
-        your career, Otofast offers a welcoming and supportive environment where
-        you can learn from others, gain new insights and perspectives, and build
-        meaningful connections with peers and mentors.
+        Welcome to <strong>Otofast Project</strong>! These terms and conditions
+        outline the rules and regulations for the use of Otofast Project's
+        Website, located at https://otofastproject.com. By accessing this
+        website we assume you accept these terms and conditions. Do not continue
+        to use Otofast Project if you do not agree to take all of the terms and
+        conditions stated on this page. The following terminology applies to
+        these Terms and Conditions, Privacy Statement and Disclaimer Notice and
+        all Agreements: "Client", "You" and "Your" refers to you, the person log
+        on this website and compliant to the Companys terms and conditions. "The
+        Company", "Ourselves", "We", "Our" and "Us", refers to our Company.
+        "Party", "Parties", or "Us", refers to both the Client and ourselves.
+        All terms refer to the offer, acceptance and consideration of payment
+        necessary to undertake the process of our assistance to the Client in
+        the most appropriate manner for the express purpose of meeting the
+        Clients needs in respect of provision of the Companys stated services,
+        in accordance with and subject to, prevailing law of Netherlands. Any
+        use of the above terminology or other words in the singular, plural,
+        capitalization and/or he/she or they, are taken as interchangeable and
+        therefore as referring to same. Cookies We employ the use of cookies. By
+        accessing Otofast Project, you agreed to use cookies in agreement with
+        the Otofast Project's Privacy Policy. Most interactive websites use
+        cookies to let us retrieve the users details for each visit. Cookies are
+        used by our website to enable the functionality of certain areas to make
+        it easier for people visiting our website. Some of our
+        affiliate/advertising partners may also use cookies. License Unless
+        otherwise stated, Otofast Project and/or its licensors own the
+        intellectual property rights for all material on Otofast Project. All
+        intellectual property rights are reserved. You may access this from
+        Otofast Project for your own personal use subjected to restrictions set
+        in these terms and conditions. You must not: Republish material from
+        Otofast Project Sell, rent or sub-license material from Otofast Project
+        Reproduce, duplicate or copy material from Otofast Project Redistribute
+        content from Otofast Project This Agreement shall begin on the date
+        hereof. Parts of this website offer an opportunity for users to post and
+        exchange opinions and information in certain areas of the website.
+        Otofast Project does not filter, edit, publish or review Comments prior
+        to their presence on the website. Comments do not reflect the views and
+        opinions of Otofast Project,its agents and/or affiliates. Comments
+        reflect the views and opinions of the person who post their views and
+        opinions. To the extent permitted by applicable laws, Otofast Project
+        shall not be liable for the Comments or for any liability, damages or
+        expenses caused and/or suffered as a result of any use of and/or posting
+        of and/or appearance of the Comments on this website. Otofast Project
+        reserves the right to monitor all Comments and to remove any Comments
+        which can be considered inappropriate, offensive or causes breach of
+        these Terms and Conditions. You warrant and represent that: You are
+        entitled to post the Comments on our website and have all necessary
+        licenses and consents to do so; The Comments do not invade any
+        intellectual property right, including without limitation copyright,
+        patent or trademark of any third party; The Comments do not contain any
+        defamatory, libelous, offensive, indecent or otherwise unlawful material
+        which is an invasion of privacy The Comments will not be used to solicit
+        or promote business or custom or present commercial activities or
+        unlawful activity. You hereby grant Otofast Project a non-exclusive
+        license to use, reproduce, edit and authorize others to use, reproduce
+        and edit any of your Comments in any and all forms, formats or media.
+        Hyperlinking to our Content The following organizations may link to our
+        Website without prior written approval: Government agencies; Search
+        engines; News organizations; Online directory distributors may link to
+        our Website in the same manner as they hyperlink to the Websites of
+        other listed businesses; and System wide Accredited Businesses except
+        soliciting non-profit organizations, charity shopping malls, and charity
+        fundraising groups which may not hyperlink to our Web site. These
+        organizations may link to our home page, to publications or to other
+        Website information so long as the link: (a) is not in any way
+        deceptive; (b) does not falsely imply sponsorship, endorsement or
+        approval of the linking party and its products and/or services; and (c)
+        fits within the context of the linking partys site. We may consider and
+        approve other link requests from the following types of organizations:
+        commonly-known consumer and/or business information sources; dot.com
+        community sites; associations or other groups representing charities;
+        online directory distributors; internet portals; accounting, law and
+        consulting firms; and educational institutions and trade associations.
+        We will approve link requests from these organizations if we decide
+        that: (a) the link would not make us look unfavorably to ourselves or to
+        our accredited businesses; (b) the organization does not have any
+        negative records with us; (c) the benefit to us from the visibility of
+        the hyperlink compensates the absence of Otofast Project; and (d) the
+        link is in the context of general resource information. These
+        organizations may link to our home page so long as the link: (a) is not
+        in any way deceptive; (b) does not falsely imply sponsorship,
+        endorsement or approval of the linking party and its products or
+        services; and (c) fits within the context of the linking partys site. If
+        you are one of the organizations listed in paragraph 2 above and are
+        interested in linking to our website, you must inform us by sending an
+        e-mail to Otofast Project. Please include your name, your organization
+        name, contact information as well as the URL of your site, a list of any
+        URLs from which you intend to link to our Website, and a list of the
+        URLs on our site to which you would like to link. Wait 2-3 weeks for a
+        response. Approved organizations may hyperlink to our Website as
+        follows: By use of our corporate name; or By use of the uniform resource
+        locator being linked to; or By use of any other description of our Web 2
+        weeks for a response. Approved organizations may hyperlink to our
+        Website as follows: By use of our corporate name; or By use of the
+        uniform resource locator being linked to; or By use of any other
+        description of our Web site being linked to that makes sense within the
+        context and format of content on the linking partys site. No use of
+        Otofast Project's logo or other artwork will be allowed for linking
+        absent a trademark license agreement. iFrames Without prior approval and
+        written permission, you may not create frames around our Webpages. We do
+        not warrant that the information on this website is complete, true, or 3
+        weeks for a response. Approved organizations may hyperlink to our Web 2
+        weeks for a response.
       </p>
       <p className="text-white">
-        Members of the community come from a diverse range of backgrounds and
-        fields, including software development, data science, cybersecurity, AI,
-        and more, making it a great place to explore different areas of interest
-        and broaden your horizons. From meetups and hackathons to online forums
-        and social media groups, Otofast offers a range of opportunities for
-        members to connect and engage with the community.
+        Approved organizations may hyperlink to our Website as follows: By use
+        of our corporate name; or By use of the uniform resource locator being
+        linked to; or By use of any other description of our Web site being
+        linked to that makes sense within the context and format of content on
+        the linking partys site. No use of Otofast Project's logo or other
+        artwork will be allowed for linking absent a trademark license
+        agreement. iFrames Without prior approval and written permission, you
+        may not create frames around our Webpages. We do not warrant that the
+        information on this website is complete, true, or 3 weeks for a
+        response. Approved organizations may hyperlink to our Web 2 weeks for a
+        response. Approved organizations may hyperlink to our Website as
+        follows: By use of our corporate name; or By use of the uniform resource
+        locator being linked to; or By use of any other description of our Web
+        site being linked to that makes sense within the context and format of
+        content on the linking partys site. No use of Otofast Project's logo or
+        other artwork will be allowed for linking absent a trademark license
+        agreement. iFrames Without prior approval and written permission, you
+        may not create frames around our Webpages. We do not warrant that the
+        information on this website is complete, true, or 3 weeks for a response
+        Approved organizations may hyperlink to our Web 2 weeks for a response.
+        Approved organizations may hyperlink to our Website as follows: By use
+        of our corporate name; or By use of the uniform resource locator being
+        linked to; or By use of any other description of our Web site being
+        linked to that makes sense within the context and format of content on
+        the linking partys site. No use of Otofast Project's logo or other
+        artwork will be allowed for linking absent a trademark license
+        agreement. iFrames Without prior approval and written permission, you
+        may not create frames around our Webpages. We do not warrant that the
+        information on this website is complete, true, or 3 weeks for a response
+        2 weeks for a response. Approved organizations may hyperlink to our Web
+        2 weeks for a response.
       </p>
 
-      <p className="text-white">
-        {" "}
-        Whether you are looking to network, collaborate on a project, or simply
-        learn more about the latest trends and technologies, Otofast is the
-        place to be for anyone passionate about tech. The entire application is
-        Developed with cool Technologie.
-      </p>
+      <p className="text-white"></p>
 
       <div>
         <h1 className="text-2xl font-medium text-[#00df9a]">Our Team </h1>
-        <p className="text-white">
-          We are a team of passionate developers, designers, and writers who are
-          committed to creating the best possible experience for our users. We
-          believe that technology should be accessible to everyone, and we
-          strive to make our products as easy to use as possible. Our goal is to
-          provide you with a platform where you can learn, share, and connect
-          with others who share your interests. We hope you enjoy using our
-          products as much as we enjoy building them! Happy Coding
-        </p>
-        
+        <p className="text-white"></p>
       </div>
     </div>
   );
