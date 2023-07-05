7377fda181ef2475b8def8c3affcac33f5b2628f
marksikaundi <emmanuelmark117@gmail.com>
2023-07-05 21:55:40 +0200

    up sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -9,6 +9,10 @@ export default function sitemap() {
       lastModified: new Date(),
     },
     {
+      url: "https://otofastproject.com/lms",
+      lastModified: new Date(),
+    },
+    {
       url: "https://otofastproject.com/features",
       lastModified: new Date(),
     },
