4484a3ae8fae52faf6faf2126a566c1c51de4fd2
marksikaundi <emmanuelmark117@gmail.com>
2023-07-08 11:45:48 +0200

    linked to the courses

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -102,7 +102,7 @@ export default function Example() {
                 </div>
                 <div className="py-6">
                   <a
-                    href="/courses"
+                    href="https://lms.devcircleafrica.com/courses"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Apply now
