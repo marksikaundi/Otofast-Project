01b3d23c58f21ab2465bdc71b303694afedb1659
marksikaundi <emmanuelmark117@gmail.com>
2023-07-09 18:53:06 +0200

    en

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">   
+    <html lang="en">   
       <body className={inter.className}>
         {/* Header */}
         {/* <Header /> */}
