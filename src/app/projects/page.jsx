b88079fdfd6bd0c621a3efee6333f62278984d33
marksikaundi <emmanuelmark117@gmail.com>
2023-07-25 22:56:07 +0200

    removed comments

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -11,15 +11,14 @@ export default function page() {
           className="w-full h-auto"
         />
       </div>
-      {/* Content on the right side */}
       <div className="flex-grow p-4">
         <h2 className="text-3xl font-bold mb-4 text-white">Otofast AI</h2>
         <p className="text-white">
           OpenAI just launched custom instructions for ChatGPT users, so they
-          don’t have to write the same instruction prompts to the chatbot every
+          don have to write the same instruction prompts to the chatbot every
           time they interact with it — inputs like “Write the answer under 1,000
           words” or “Keep the tone of response formal.” The company said this
-          feature lets you “share anything you’d like ChatGPT to consider in its
+          feature lets you “share anything you like ChatGPT to consider in its
           response.” For example, a teacher can say they are teaching
           fourth-grade math or a developer can specify the code language they
           prefer when asking for suggestions. A person can also specify their
@@ -27,12 +26,14 @@ export default function page() {
           vacation planning accordingly. While users can already specify these
           things while chatting with the bot, custom instructions are helpful if
           users need to set the same context frequently.
-          <a href="https://otofast.vercel.app" className="font-semibold text-indigo-600">
+          <a
+            href="https://otofast.vercel.app"
+            className="font-semibold text-indigo-600"
+          >
             <span className="absolute inset-0" aria-hidden="true" />
-              Check Demo <span aria-hidden="true">&rarr;</span>
+            Check Demo <span aria-hidden="true">&rarr;</span>
           </a>
         </p>
-        {/* Add more content here */}
       </div>
     </div>
   );
