0fc796e15037bc177c64b74d5deb417875b5b5bc
marksikaundi <emmanuelmark117@gmail.com>
2023-07-26 20:52:19 +0200

    fixed changes

--- src/app/features/page.jsx
+++ src/app/features/page.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
     <div>
-      Features
+      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Company retreats
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Incredible accommodation for your team
+            </a>
+            <p class="mt-2 text-slate-500">
+              Looking to take your team away on a retreat to enjoy awesome food
+              and take in some sunshine? We have a list of places to do just
+              that.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
